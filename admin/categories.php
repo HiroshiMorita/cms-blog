81d6872100923606b17d67a66cb6279f1740691d
HiroshiMorita <ikimonogakaru@gmail.com>
2019-10-29 19:00:56 +0900

    refactored

--- admin/categories.php
+++ admin/categories.php
@@ -3,7 +3,7 @@
     <div id="wrapper">
 
         <!-- Navigation -->
-        <?php include "includes/admin_navigation.php" ?>
+<?php include "includes/admin_navigation.php" ?>
 
         <div id="page-wrapper">
 
@@ -17,24 +17,7 @@
                             <small>Author</small>
                         </h1>
                         <div class="col-xs-6">
-
-                          <?php
-                          if(isset($_POST['submit'])) {
-                            $cat_title = $_POST['cat_title'];
-                            if($cat_title == "" || empty($cat_title)) {
-                              echo "This field should not be empty";
-                            } else {
-                              $query = "INSERT INTO categories(cat_title) ";
-                              $query .= "VALUE('{$cat_title}') ";
-                              $create_category_query = mysqli_query($connection, $query);
-                              if(!$create_category_query) {
-                                die('QUERY FAILED' . mysqli_error($connection));
-                              }
-                            }
-                          }
-                          ?>
-
-
+<?php insert_categories() ?>
                           <form action="" method="post">
                             <div class="form-group">
                               <label for="cat-title"> Add Category</label>
@@ -44,12 +27,13 @@
                               <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                           </form>
-                          <?php
-                          if(isset($_GET['edit'])) {
-                            $cat_id = $_GET['edit'];
-                            include "includes/update_categories.php";
-                          }
-                          ?>
+
+<?php //UPDATE AND INCLUDE QUERY
+if(isset($_GET['edit'])) {
+$cat_id = $_GET['edit'];
+include "includes/update_categories.php";
+}
+?>
 
                         </div>
                         <div class="col-xs-6">
@@ -62,29 +46,9 @@
                               </tr>
                             </thread>
                             <tbody>
-<?php  //FIND ALL CATEGORIES QUERY
-$query ="SELECT * FROM categories";
-$select_categories = mysqli_query($connection,$query);
-while($row = mysqli_fetch_assoc($select_categories)) {
-$cat_id = $row['cat_id'];
-$cat_title = $row['cat_title'];
-echo "<tr>";
-echo "<td>{$cat_id}</td>";
-echo "<td>{$cat_title}</td>";
-echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-echo "</tr>";
-}
-?>
+<?php findAllCategories() ?>
 
-<?php //DELETE QUERY
-if(isset($_GET['delete'])){
-$the_cat_id = $_GET['delete'];
-$query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-$delete_query = mysqli_query($connection, $query);
-header("Location: categories.php"); //categories.phpに処理後リダイレクトしている。
-}
-?>
+<?php deleteCategories() ?>
                             </tbody>
                           </table>
                         </div>
