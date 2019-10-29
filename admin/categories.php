a586b5f5e3f382f62d0beb800e3f9338448271a3
HiroshiMorita <ikimonogakaru@gmail.com>
2019-10-29 18:12:37 +0900

    update category 実装

--- admin/categories.php
+++ admin/categories.php
@@ -24,15 +24,16 @@
                             if($cat_title == "" || empty($cat_title)) {
                               echo "This field should not be empty";
                             } else {
-                              $query = "INSERT INTO categories(cat_title)";
-                              $query .= "VALUE('{$cat_title}')";
-                              $create_category_query = mysqli_query($connection,$query);
-                              if(!create_category_query) {
+                              $query = "INSERT INTO categories(cat_title) ";
+                              $query .= "VALUE('{$cat_title}') ";
+                              $create_category_query = mysqli_query($connection, $query);
+                              if(!$create_category_query) {
                                 die('QUERY FAILED' . mysqli_error($connection));
                               }
                             }
                           }
                           ?>
+
 
                           <form action="" method="post">
                             <div class="form-group">
@@ -43,13 +44,16 @@
                               <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                           </form>
+                          <?php
+                          if(isset($_GET['edit'])) {
+                            $cat_id = $_GET['edit'];
+                            include "includes/update_categories.php";
+                          }
+                          ?>
+
                         </div>
                         <div class="col-xs-6">
-<?php
-$query ="SELECT * FROM categories";
-$select_categories =
-mysqli_query($connection,$query);
-?>
+
                           <table class="table table-bordered table-hover">
                             <thread>
                               <tr>
@@ -58,15 +62,27 @@ mysqli_query($connection,$query);
                               </tr>
                             </thread>
                             <tbody>
-<?php
-while($row =
-mysqli_fetch_assoc($select_categories)) {
+<?php  //FIND ALL CATEGORIES QUERY
+$query ="SELECT * FROM categories";
+$select_categories = mysqli_query($connection,$query);
+while($row = mysqli_fetch_assoc($select_categories)) {
 $cat_id = $row['cat_id'];
 $cat_title = $row['cat_title'];
 echo "<tr>";
 echo "<td>{$cat_id}</td>";
 echo "<td>{$cat_title}</td>";
+echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
 echo "</tr>";
+}
+?>
+
+<?php //DELETE QUERY
+if(isset($_GET['delete'])){
+$the_cat_id = $_GET['delete'];
+$query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
+$delete_query = mysqli_query($connection, $query);
+header("Location: categories.php"); //categories.phpに処理後リダイレクトしている。
 }
 ?>
                             </tbody>
