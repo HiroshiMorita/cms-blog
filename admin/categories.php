a1ad5a560ca8797bc4aea3bf7d8e82bc5d1ca8b4
HiroshiMorita <ikimonogakaru@gmail.com>
2019-11-02 17:25:26 +0900

    add delete-post,create-post function

--- admin/categories.php
+++ admin/categories.php
@@ -11,7 +11,7 @@
 
                 <!-- Page Heading -->
                 <div class="row">
-                    <div class="col-lg-12"></div>
+                    <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome to admin
                             <small>Author</small>
@@ -39,12 +39,12 @@ include "includes/update_categories.php";
                         <div class="col-xs-6">
 
                           <table class="table table-bordered table-hover">
-                            <thread>
+                            <thead>
                               <tr>
                                 <th>ID</th>
                                 <th>Category Title</th>
                               </tr>
-                            </thread>
+                            </thead>
                             <tbody>
 <?php findAllCategories() ?>
 
