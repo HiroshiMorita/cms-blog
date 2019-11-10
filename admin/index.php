f1df9da8ad5fedb157c97ab022b68437b82da689
HiroshiMorita <ikimonogakaru@gmail.com>
2019-11-10 22:10:28 +0900

    ダッシュボードカウント機能実装

--- admin/index.php
+++ admin/index.php
@@ -43,7 +43,13 @@
                         <i class="fa fa-file-text fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                  <div class='huge'>12</div>
+                        <?php
+                            $query = "SELECT * FROM posts";
+                            $select_all_posts = mysqli_query($connection, $query);
+                            $post_count = mysqli_num_rows($select_all_posts);
+                            echo "<div class='huge'>{$post_count}</div>";
+                        ?>
+
                         <div>Posts</div>
                     </div>
                 </div>
@@ -65,8 +71,13 @@
                         <i class="fa fa-comments fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                     <div class='huge'>23</div>
-                      <div>Comments</div>
+                    <?php
+                        $query = "SELECT * FROM comments";
+                        $select_all_comments = mysqli_query($connection, $query);
+                        $comment_count = mysqli_num_rows($select_all_comments);
+                        echo "<div class='huge'>{$comment_count}</div>";
+                    ?>
+                        <div>Comments</div>
                     </div>
                 </div>
             </div>
@@ -87,7 +98,12 @@
                         <i class="fa fa-user fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                    <div class='huge'>23</div>
+                        <?php
+                            $query = "SELECT * FROM users";
+                            $select_all_users = mysqli_query($connection, $query);
+                            $user_count = mysqli_num_rows($select_all_users);
+                            echo "<div class='huge'>{$user_count}</div>";
+                        ?>
                         <div> Users</div>
                     </div>
                 </div>
@@ -109,8 +125,13 @@
                         <i class="fa fa-list fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                        <div class='huge'>13</div>
-                         <div>Categories</div>
+                        <?php
+                            $query = "SELECT * FROM categories";
+                            $select_all_categories = mysqli_query($connection, $query);
+                            $category_count = mysqli_num_rows($select_all_categories);
+                            echo "<div class='huge'>{$category_count}</div>";
+                        ?>
+                        <div>Categories</div>
                     </div>
                 </div>
             </div>
@@ -125,6 +146,32 @@
     </div>
 </div>
                 <!-- /.row -->
+
+<div class="">
+<script type="text/javascript">
+    google.charts.load('current', {'packages':['bar']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Date', 'Count'],
+            ['Posts', 1000],
+        ]);
+
+        var options = {
+            chart: {
+                title: 'Company Performance',
+                subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+            }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+    }
+    </script>
+    <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+</div>
 
             </div>
             <!-- /.container-fluid -->
