6e7d6c4e885244c5ddd4cc54d5ceb8e76ee9244c
HiroshiMorita <ikimonogakaru@gmail.com>
2019-11-11 05:54:36 +0900

    棒グラフ追加

--- admin/index.php
+++ admin/index.php
@@ -31,9 +31,9 @@
                 </div>
                 <!-- /.row -->
 
-       
+
                 <!-- /.row -->
-                
+
                 <div class="row">
     <div class="col-lg-3 col-md-6">
         <div class="panel panel-primary">
@@ -146,6 +146,19 @@
     </div>
 </div>
                 <!-- /.row -->
+<?php
+$query = "SELECT * FROM posts WHERE post_status = 'draft'";
+$select_all_draft_posts = mysqli_query($connection, $query);
+$post_draft_count = mysqli_num_rows($select_all_draft_posts);
+
+$query = "SELECT * FROM comments WHERE comment_status = 'unapproved'";
+$unapproved_comments_query = mysqli_query($connection, $query);
+$unapproved_comment_count = mysqli_num_rows($unapproved_comments_query);
+
+$query = "SELECT * FROM users WHERE user_role = 'subscriber'";
+$select_all_subscribers = mysqli_query($connection, $query);
+$subscriber_count = mysqli_num_rows($select_all_subscribers);
+?>
 
 <div class="">
 <script type="text/javascript">
@@ -155,7 +168,14 @@
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
             ['Date', 'Count'],
-            ['Posts', 1000],
+            <?php
+            $element_text = ['Active Posts', 'Draft Posts', 'Comments', 'Pending Comments', 'Users', 'Subscribers', 'Categories'];
+            $element_count = [$post_count, $post_draft_count, $comment_count, $unapproved_comment_count, $user_count, $subscriber_count, $category_count];
+            $number_of_element = count($element_text);
+            for($i=0; $i<$number_of_element; $i++) {
+                echo "['{$element_text[$i]}'".","." {$element_count[$i]}], ";
+            }
+            ?>
         ]);
 
         var options = {
