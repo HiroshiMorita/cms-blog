bd0dd23e99ac4083f46df430450dec23437f0bfd
HiroshiMorita <ikimonogakaru@gmail.com>
2019-11-15 20:23:01 +0900

    docker対応

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,19 @@
 <?php
+//MAMP用
+// $db['db_host'] = "localhost";
+// $db['db_user'] = "root";
+// $db['db_pass'] = "root";
+// $db['db_name'] = "cms-blog";
 
-$db['db_host'] = "localhost";
+// foreach($db as $key => $value){
+// define(strtoupper($key), $value);
+// }
+
+// $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+
+//Docker-LEMP用
+$db['db_host'] = "db";
 $db['db_user'] = "root";
 $db['db_pass'] = "root";
 $db['db_name'] = "cms-blog";
@@ -10,6 +23,20 @@ define(strtoupper($key), $value);
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+try {
+  $dsn = 'mysql:host=db;dbname=cms-blog;';
+  $db = new PDO($dsn, 'root', 'root');
+
+  $sql = 'SELECT version();';
+  $stmt = $db->prepare($sql);
+  $stmt->execute();
+  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  var_dump($result);
+} catch (PDOException $e) {
+  echo $e->getMessage();
+  exit;
+}
 
 // if($connection) {
 //   echo "connected";
